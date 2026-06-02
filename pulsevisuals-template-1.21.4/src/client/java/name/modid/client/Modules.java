818eafbf47445c5c0e70fea34423571a911d4f40
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 15:39:03 +0700

    Create Modules.java

---
+++ pulsevisuals-template-1.21.4/src/client/java/name/modid/client/Modules.java
@@ -0,0 +1,12 @@
+package name.modid;
+
+public class Modules {
+    // Состояния функций (true - включен, false - выключен)
+    public static boolean autoSprint = false;
+    public static boolean esp = false;
+    public static boolean jumpCircles = false;
+    
+    // Координаты нашего HUD хаба на экране (по умолчанию левый верхний угол)
+    public static int hudX = 5;
+    public static int hudY = 5;
+}
