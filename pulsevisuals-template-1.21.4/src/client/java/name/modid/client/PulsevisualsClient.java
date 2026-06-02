a494a0804ebec77e5d9fc02ec8c4614b578e5cb8
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 16:24:03 +0700

    Update PulsevisualsClient.java

--- pulsevisuals-template-1.21.4/src/client/java/name/modid/client/PulsevisualsClient.java
+++ pulsevisuals-template-1.21.4/src/client/java/name/modid/client/PulsevisualsClient.java
@@ -3,20 +3,32 @@ package name.modid.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.KeyBinding;
 import net.minecraft.client.util.InputConstants;
 import name.modid.VisualsMenuScreen;
-import name.modid.HudRenderer;
 import name.modid.KillAura;
 import org.lwjgl.glfw.GLFW;
 
 public class PulsevisualsClient implements ClientModInitializer {
+    // Создаем переменную для клавиши
+    public static KeyBinding openMenuKey;
+
     @Override
     public void onInitializeClient() {
-        HudRenderCallback.EVENT.register(new HudRenderer());
+        // Регистрируем клавишу (Right Shift)
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.pulsevisuals.open",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_SHIFT, 
+            "category.pulsevisuals.main"
+        ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            // Проверка нажатия клавиши для открытия GUI
+            if (openMenuKey.consumeClick()) {
+                client.setScreen(new VisualsMenuScreen());
+            }
+            // Работа KillAura
             KillAura.onTick(client);
         });
     }
