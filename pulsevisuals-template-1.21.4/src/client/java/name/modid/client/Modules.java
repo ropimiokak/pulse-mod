0f065fafa80eddf0b98146f64aecdfd4c66b5a35
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 16:29:31 +0700

    Update Modules.java

--- pulsevisuals-template-1.21.4/src/client/java/name/modid/client/Modules.java
+++ pulsevisuals-template-1.21.4/src/client/java/name/modid/client/Modules.java
@@ -1,35 +1,10 @@
 package name.modid;
 
 public class Modules {
-    // Вспомогательные функции
     public static boolean autoSprint = false;
     public static boolean killAura = false;
     
-    // Координаты элементов на экране
-    public static int hudX = 10, hudY = 10;
-    public static int armorX = 10, armorY = 100;
-    public static int potionX = 10, potionY = 150;
-
-    // Настройки KillAura (обходы)
-    public static double auraRange = 3.2;
-}
-public static int fpsX = 10, fpsY = 200;
-public static int xyzX = 10, xyzY = 230;
-package name.modid;
-
-public class Modules {
     // Настройки Aura
-    public static double auraRange = 3.2;
-    public static boolean auraPlayers = true;
-    public static boolean auraMobs = false;
-    public static String auraMode = "Legit"; // "Legit", "FT" (FunTime), "ST" (SpookyTime)
-}
-package name.modid;
-
-public class Modules {
-    public static boolean autoSprint = false;
-    public static boolean killAura = false;
-    // Настройки ауры
     public static double auraRange = 3.2;
     public static String auraMode = "Legit"; 
     
