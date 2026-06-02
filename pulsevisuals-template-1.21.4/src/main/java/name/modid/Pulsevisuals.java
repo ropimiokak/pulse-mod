0886cae0dbde27a66681d380ccdf4498db0febef
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 14:00:15 +0700

    Update Pulsevisuals.java

--- pulsevisuals-template-1.21.4/src/main/java/name/modid/Pulsevisuals.java
+++ pulsevisuals-template-1.21.4/src/main/java/name/modid/Pulsevisuals.java
@@ -7,14 +7,12 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-// ВНИМАНИЕ: Если твой файл называется PulseVisuals.java,
-// то вместо ExampleMod ниже напиши PulseVisuals
-public class ExampleMod implements ClientModInitializer {
+public class Pulsevisuals implements ClientModInitializer {
     private static KeyBinding openMenuKey;
 
     @Override
     public void onInitializeClient() {
-        // Привязка функции к Правому Шифту
+        // Регистрируем кнопку открытия меню на Правый Шифт
         openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.pulsevisuals.open",
                 InputUtil.Type.KEYSYM,
@@ -22,10 +20,11 @@ public class ExampleMod implements ClientModInitializer {
                 "category.pulsevisuals.main"
         ));
 
-        // Проверка нажатия кнопки каждый тик
+        // Проверяем нажатие каждый тик игры
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player != null) {
                 while (openMenuKey.wasPressed()) {
+                    // Открываем наше меню
                     client.setScreen(new VisualsMenuScreen());
                 }
             }
