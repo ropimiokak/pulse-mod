1b85ed6cbec844d0ebbdb64311e8c0ad5cd33674
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 15:40:30 +0700

    Update PulsevisualsClient.java

--- pulsevisuals-template-1.21.4/src/client/java/name/modid/client/PulsevisualsClient.java
+++ pulsevisuals-template-1.21.4/src/client/java/name/modid/client/PulsevisualsClient.java
@@ -3,6 +3,7 @@ package name.modid;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.KeyMapping;
 import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
@@ -19,12 +20,23 @@ public class PulsevisualsClient implements ClientModInitializer {
                 "category.pulsevisuals.main"
         ));
 
+        // Логика игровых тиков
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player != null) {
                 while (openMenuKey.consumeClick()) {
                     client.setScreen(new VisualsMenuScreen());
                 }
+                
+                // РАБОТА ФУНКЦИИ АВТО-СПРИНТ
+                if (Modules.autoSprint) {
+                    if (client.player.input.up && !client.player.isPassenger()) {
+                        client.player.setSprinting(true);
+                    }
+                }
             }
         });
+
+        // РЕГИСТРАЦИЯ НАШЕГО КАСТОМНОГО HUD ХАБА
+        HudRenderCallback.EVENT.register(new HudRenderer());
     }
 }
