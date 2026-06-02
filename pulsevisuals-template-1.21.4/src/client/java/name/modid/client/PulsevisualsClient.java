68524fd261031c0e018b2b991fcbc85a22b413e1
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 15:11:10 +0700

    Update PulsevisualsClient.java

--- pulsevisuals-template-1.21.4/src/client/java/name/modid/client/PulsevisualsClient.java
+++ pulsevisuals-template-1.21.4/src/client/java/name/modid/client/PulsevisualsClient.java
@@ -1,10 +1,30 @@
-package name.modid.client;
+package name.modid;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import com.mojang.blaze3d.platform.InputConstants;
+import org.lwjgl.glfw.GLFW;
 
 public class PulsevisualsClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    private static KeyMapping openMenuKey;
+
+    @Override
+    public void onInitializeClient() {
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.pulsevisuals.open",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT, 
+                "category.pulsevisuals.main"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player != null) {
+                while (openMenuKey.consumeClick()) {
+                    client.setScreen(new VisualsMenuScreen());
+                }
+            }
+        });
+    }
 }
