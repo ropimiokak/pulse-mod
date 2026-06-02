43a04ee62f6cde721b83ec6aba171721f70b0051
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 15:57:11 +0700

    Update PulsevisualsClient.java
    Fix movement input for 1.21.4

--- pulsevisuals-template-1.21.4/src/client/java/name/modid/client/PulsevisualsClient.java
+++ pulsevisuals-template-1.21.4/src/client/java/name/modid/client/PulsevisualsClient.java
@@ -1,42 +1,48 @@
-package name.modid;
+package name.modid.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.KeyMapping;
-import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyBinding;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.util.InputConstants;
+import name.modid.VisualsMenuScreen;
+import name.modid.HudRenderer;
+import name.modid.Modules;
 import org.lwjgl.glfw.GLFW;
 
 public class PulsevisualsClient implements ClientModInitializer {
-    private static KeyMapping openMenuKey;
+    private static KeyBinding openMenuKey;
 
     @Override
     public void onInitializeClient() {
-        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.pulsevisuals.open",
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_RIGHT_SHIFT, 
-                "category.pulsevisuals.main"
+        // Регистрируем HUD-отрисовщик
+        HudRenderCallback.EVENT.register(new HudRenderer());
+
+        // Регистрируем кнопку открытия меню на Правый Шифт
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.pulsevisuals.open",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_SHIFT,
+            "category.pulsevisuals.main"
         ));
 
-        // Логика игровых тиков
+        // Логика тиков (AutoSprint и открытие меню)
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null) {
-                while (openMenuKey.consumeClick()) {
-                    client.setScreen(new VisualsMenuScreen());
-                }
-                
-                // РАБОТА ФУНКЦИИ АВТО-СПРИНТ
-                if (Modules.autoSprint) {
-                    if (client.player.input.up && !client.player.isPassenger()) {
-                        client.player.setSprinting(true);
-                    }
+            if (client.player == null) return;
+
+            // Открытие ClickGUI меню
+            while (openMenuKey.wasPressed()) {
+                client.setScreen(new VisualsMenuScreen());
+            }
+
+            // ИСПРАВЛЕНО ДЛЯ 1.21.4: Используем pressingForward вместо input.up
+            if (Modules.autoSprint) {
+                if (client.player.input.pressingForward && !client.player.isPassenger()) {
+                    client.player.setSprinting(true);
                 }
             }
         });
-
-        // РЕГИСТРАЦИЯ НАШЕГО КАСТОМНОГО HUD ХАБА
-        HudRenderCallback.EVENT.register(new HudRenderer());
     }
 }
