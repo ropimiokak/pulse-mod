e6af520b12504e828f01387e9e53f1b7cc7ce84d
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 16:15:30 +0700

    Update Modules.java

--- pulsevisuals-template-1.21.4/src/client/java/name/modid/client/Modules.java
+++ pulsevisuals-template-1.21.4/src/client/java/name/modid/client/Modules.java
@@ -1,12 +1,15 @@
 package name.modid;
 
 public class Modules {
-    // Состояния функций (true - включен, false - выключен)
+    // Вспомогательные функции
     public static boolean autoSprint = false;
-    public static boolean esp = false;
-    public static boolean jumpCircles = false;
+    public static boolean killAura = false;
     
-    // Координаты нашего HUD хаба на экране (по умолчанию левый верхний угол)
-    public static int hudX = 5;
-    public static int hudY = 5;
+    // Координаты элементов на экране
+    public static int hudX = 10, hudY = 10;
+    public static int armorX = 10, armorY = 100;
+    public static int potionX = 10, potionY = 150;
+
+    // Настройки KillAura (обходы)
+    public static double auraRange = 3.2;
 }
