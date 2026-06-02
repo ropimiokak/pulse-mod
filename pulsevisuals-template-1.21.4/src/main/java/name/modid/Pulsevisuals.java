084796134a588e029a36359fef2cb1ca10d76b16
ropimiokak <roma.kholudeev@mail.ru>
2026-06-02 15:06:41 +0700

    Update Pulsevisuals.java

--- pulsevisuals-template-1.21.4/src/main/java/name/modid/Pulsevisuals.java
+++ pulsevisuals-template-1.21.4/src/main/java/name/modid/Pulsevisuals.java
@@ -3,28 +3,25 @@ package name.modid;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.KeyMapping;
+import com.mojang.blaze3d.platform.InputConstants;
 import org.lwjgl.glfw.GLFW;
 
 public class Pulsevisuals implements ClientModInitializer {
-    private static KeyBinding openMenuKey;
+    private static KeyMapping openMenuKey;
 
     @Override
     public void onInitializeClient() {
-        // Регистрируем кнопку открытия меню на Правый Шифт
-        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.pulsevisuals.open",
-                InputUtil.Type.KEYSYM,
+                InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_RIGHT_SHIFT, 
                 "category.pulsevisuals.main"
         ));
 
-        // Проверяем нажатие каждый тик игры
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player != null) {
-                while (openMenuKey.wasPressed()) {
-                    // Открываем наше меню
+                while (openMenuKey.consumeClick()) {
                     client.setScreen(new VisualsMenuScreen());
                 }
             }
